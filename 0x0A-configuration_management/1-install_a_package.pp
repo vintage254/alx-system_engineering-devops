ed20432e25dfbbef71daccba7d3b8c0048459d83
“vintage254” <derricknjuguna414@gmail.com>
2024-04-19 13:58:57 -0700

    flask installation script

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,12 @@
-#!/usr/bin/pup
-# install flask verssion 2.1.0
-package{ 'flask':
-  ensure => '2.1.0',
-  provider => 'pip3'
+package { 'python3-pip':
+  ensure => installed,
 }
+
+exec { 'install_flask':
+  command     => '/usr/bin/pip3 install Flask==2.1.0',
+  path        => ['/usr/bin'],
+  environment => 'LANG=en_US.UTF-8',
+  unless      => '/usr/bin/flask --version | grep "Flask 2.1.0"',
+  require     => Package['python3-pip'],
+}
+

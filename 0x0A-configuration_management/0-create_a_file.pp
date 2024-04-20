a30cd8ab472114da8057bdcdc7c233f0095bff90
“vintage254” <derricknjuguna414@gmail.com>
2024-04-20 03:01:44 -0700

    fix

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,6 @@
-# Ensure directory exists
-file { '/tmp':
-  ensure => 'directory',
-}
 # create a file 
 file { '/tmp/school':
+  ensure  => file,
   mode    => '0744',
   owner   => 'www-data'
   group   => 'www-data'
